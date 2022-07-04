bb2e0d8ef6419f2728a34cd975d4bcc5079084d3
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2022-07-04 16:39:01 +0430

    add hws, midterm, and final

---
+++ Midterm-14002/2.py
@@ -0,0 +1,14 @@
+def is_magical(t):
+    item = t[0]
+    list_ = t[1]
+    visited = dict()
+    # your code goes here
+    return False
+
+
+if __name__ == '__main__':
+    assert is_magical((3, [1, 5, 2, 7, 12, 5]))
+    assert is_magical((7, [3, 23, 4]))
+    assert is_magical((-3, [-17, 12, 14, 103]))
+    assert not is_magical((4, [5, 1, 2, 1]))
+    assert not is_magical((6, [5, 2, 7, 17]))
